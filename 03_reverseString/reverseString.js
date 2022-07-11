0e005ea329303bae20070ab2b54d6436c677b8ba
cybr-coder <codercybr@gmail.com>
2022-07-11 15:11:43 -0400

    Exercise Two Passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let revStr = '';
+    for (i = string.length -1; i >= 0; i-- ) {
+        revStr += string[i];
+    }
+    return revStr;
 };
 
 // Do not edit below this line
