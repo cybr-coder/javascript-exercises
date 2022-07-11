1580ec828d979925dbaf008aff429191249743a3
cybr-coder <codercybr@gmail.com>
2022-07-11 14:20:50 -0400

    Exercise One Passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const times = Math.floor(Math.random() * 1000)
+const repeatString = function(word, times ) {
+    if (times < 0) {
+        return 'ERROR';
+    }
+    string = '';
+    for (let i = 0; i < times; i++) {
+        string += word;
+    }
+   return string;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
