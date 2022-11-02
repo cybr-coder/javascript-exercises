682a767b31600cae51901825fd89013480c87376
cybr-coder <codercybr@gmail.com>
2022-11-02 08:13:06 -0400

    Exercise Five Passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let totalSum = 0;
+    let error = 'ERROR';
+    let biggerNum = Math.max(a, b);
+    let smallerNum = Math.min(a, b);
+    for (let i = smallerNum; i <= biggerNum; i++) {
+        totalSum += i;
+    }
+    if (biggerNum < 0 || smallerNum < 0) {
+        return error;
+    } else if (isNaN(biggerNum) || isNaN(smallerNum)) {
+        return error;
+    }
+    return totalSum;
+}
 
-};
 
 // Do not edit below this line
 module.exports = sumAll;
