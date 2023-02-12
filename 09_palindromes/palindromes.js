c5f9f1162ddfde36e72ef0912c1e06837e3b8c98
cybr-coder <codercybr@gmail.com>
2023-02-12 18:12:30 +0000

    Exercise Nine Passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	let reMove = /[^A-Za-z0-9]/g;
+	let lowerCase = str.toLowerCase().replace(reMove, '');
+	let reverseStr = lowerCase.split('').reverse().join('');
+	return reverseStr === lowerCase;
 };
 
 // Do not edit below this line
